a28fe4d654279b1cdc8e34534ac927f3f739e09d
Philipp Grabowski <grabowski_philipp@web.de>
2025-09-03 16:25:15 +0200

    addPrintFunction

--- 3.6.2_Objekte&Konstruktoren/Bonus/aufgabe6.dart
+++ 3.6.2_Objekte&Konstruktoren/Bonus/aufgabe6.dart
@@ -11,11 +11,15 @@ class Book {
   final Author author;
 
   Book({required this.title, required this.pageNumber, required this.author});
+
+  void printInfo() {
+    print('Titel des Buches: ${this.title}, Seitenanzahl: ${this.pageNumber} und Name des Autors ist ${this.author.name}.');
+  }
 }
 
 void main(List<String> args) {
   Author author = Author(name: 'J. K. Rowling', yearOfBirth: '1975');
   Book book = Book(title: 'Harry Potter', pageNumber: 800, author: author);
 
-  print('Titel des Buches: ${book.title}, Seitenanzahl: ${book.pageNumber} und Name des Autors ist ${book.author}');
+  book.printInfo();
 }
