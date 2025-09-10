e436ab722b5ec70346f73f23116f618282f6dbdd
Philipp Grabowski <grabowski_philipp@web.de>
2025-09-10 11:09:40 +0200

    addNewTask

---
+++ 4.2.2_Flutter_Intro/Bonus/aufgabe1.dart
@@ -0,0 +1,41 @@
+class Widget {}
+
+class MaterialApp extends Widget {
+  Widget? home;
+
+  MaterialApp({this.home});
+}
+
+class Scaffold extends Widget {
+  Widget? body;
+
+  Scaffold({this.body});
+}
+
+class Center extends Widget {
+  Widget? child;
+
+  Center({this.child});
+}
+
+class Column extends Widget {
+  List<Widget>? children;
+
+  Column({this.children});
+}
+
+class Text extends Widget {
+  String text;
+
+  Text(this.text);
+}
+
+void main(List<String> args) {
+  Widget myApp = MaterialApp(
+    home: Scaffold(
+      body: Center(child: Column(children: [Text('Hallo'), Text('Welt')])),
+    ),
+  );
+
+  print(myApp);
+}
