9452d6b3ddf025975d10c1027f0a469737523ddd
Philipp Grabowski <grabowski_philipp@web.de>
2025-09-11 17:35:40 +0200

    deleteList

--- flutterDailyTasks/daily_tasks/lib/main.dart
+++ flutterDailyTasks/daily_tasks/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:daily_tasks/hello_world/hello_world.dart';
 import 'package:flutter/material.dart';
 
-List<String> texts = ['Test1', 'Text', 'Text3', 'Text4'];
-
 void main() {
   runApp(MaterialApp(home: HelloWorld()));
 }
